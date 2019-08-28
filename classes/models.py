005999482ef724213931037ed40264934219d4f8
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-08-29 00:58:25 +0300

    finish miniclassrooms

--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Classroom(models.Model):
-	name = models.CharField(max_length=120)
-	subject = models.CharField(max_length=120)
-	year = models.IntegerField()
+    name = models.CharField(max_length=120)
+    subject = models.CharField(max_length=120)
+    year = models.IntegerField()
 
-	def get_absolute_url(self):
-		return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+    teacher = models.ForeignKey(User , on_delete = models.CASCADE,related_name='classrooms')
+
+    def get_absolute_url(self):
+        return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+
+CHOICES = [
+        ('male','Male'),
+        ('female', 'Female'),
+]
+class Student(models.Model):
+    name = models.CharField(max_length=120)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=7, choices= CHOICES , default='male')
+    exam_grade = models.DecimalField(max_digits=5, decimal_places=2)
+    classroom = models.ForeignKey(Classroom , on_delete = models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return (self.name)
+    
